3fed68f45553bd149e765d1109bbd08dfc4a19ea
Sasha Ocheev <OcheevSasha@gmail.com>
2016-02-24 23:00:54 +0300

    lab1. fix reading я char from file and replacing 1231234 in 12312312345

--- l1/lab1/lab1/lab1.cpp
+++ l1/lab1/lab1/lab1.cpp
@@ -2,56 +2,22 @@
 //
 
 #include "stdafx.h"
-#include <conio.h>
 
-static const std::streamoff MAX_SIZE = 2147483648;
-
-std::streamoff GetFileSize(std::string fileName)
+void ReplaceSubstringInFile(const std::string searchString, const std::string replaceString, std::ifstream &inputFile, std::ofstream &outputFile)
 {
-	std::streamoff size = -1;
-	std::ifstream file(fileName, std::ios_base::binary);
-	file.seekg(0, std::ios::end);
-	size = file.tellg();
-	return size;
-}
-
-int main(int argc, char *argv[])
-{
-	if (argc != 5)
-	{
-		std::cout << "invalid input format" << std::endl;
-		std::cout << "enter <input file> <output file> <search string> <replace string>" << std::endl;
-		return 1;
-	}
-
-	std::streamoff size = GetFileSize(argv[1]);
-	if (size == -1)
-	{
-		std::cout << "file can't be opened" << std::endl;
-		return 2;
-	}
-	else if (size > MAX_SIZE)
-	{
-		std::cout << "size of file is more than 2 GB" << std::endl;
-		return 3;
-	}
-	
-	std::ifstream inputFile(argv[1], std::ios_base::in);
-	std::ofstream outputFile(argv[2], std::ios_base::out | std::ios_base::trunc);
-
-	const std::string searchString(argv[3]);
-	const std::string replaceString(argv[4]);
-
 	std::string tempString;
 	char tempChar;
-	while ((tempChar = inputFile.get()) != EOF)
-	{		
+	while (inputFile.get(tempChar))
+	{
 		tempString += tempChar;
 
-		if (!searchString.length() || tempString.back() != searchString[tempString.length() - 1])
+		if (searchString.empty() || tempString.back() != searchString[tempString.length() - 1])
 		{
-			outputFile << tempString;
-			tempString.clear();
+			while (!tempString.empty() && tempString.back() != searchString[tempString.length() - 1])
+			{
+				outputFile << tempString.front();
+				tempString.erase(tempString.begin());
+			}
 		}
 		else if (tempString.length() == searchString.length())
 		{
@@ -59,14 +25,36 @@ int main(int argc, char *argv[])
 			tempString.clear();
 		}
 	}
-	if (tempString.size())
-		outputFile << tempString;
+	outputFile << tempString;
+}
+
+int main(int argc, char *argv[])
+{
+	setlocale(LC_ALL, "Russian");
+
+	if (argc != 5)
+	{
+		std::cout << "invalid input format" << std::endl;
+		std::cout << "enter <input file> <output file> <search string> <replace string>" << std::endl;
+		return 1;
+	}
+
+	std::ifstream inputFile(argv[1], std::ios_base::in);
+	if (!inputFile.is_open())
+	{
+		std::cout << "input file can't be opened" << std::endl;
+		return 2;
+	}
+
+	std::ofstream outputFile(argv[2], std::ios_base::out | std::ios_base::trunc);
+
+	ReplaceSubstringInFile(argv[3], argv[4], inputFile, outputFile);
 
 	outputFile.flush();
 	if (!outputFile)
 	{
 		std::cout << "an error occurred when writing outputFile" << std::endl;
-		return 4;
+		return 3;
 	}
 
     return 0;
