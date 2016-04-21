9a6e34e39c45a04eec45678ca23c8eb335dc7a09
Sasha Ocheev <OcheevSasha@gmail.com>
2016-04-21 04:13:26 +0300

    исправлены тесты

--- lab2/FindAndReplace/Tests/Tests.cpp
+++ lab2/FindAndReplace/Tests/Tests.cpp
@@ -11,73 +11,79 @@ BOOST_AUTO_TEST_SUITE(FindAndReplace_test)
 	struct Fixture
 	{
 		std::string source = "sometimes happy, sometimes sad";
+		std::string expected;
 	};
 
 	BOOST_FIXTURE_TEST_CASE(replace_empty_to_not_empty, Fixture)
 	{
+		expected = source;
 		auto result = FindAndReplace(source, "", "asd");
-		BOOST_CHECK_EQUAL_COLLECTIONS(source.begin(), source.end(), result.begin(), result.end());
+		BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), result.begin(), result.end());
 	}
 
 	BOOST_FIXTURE_TEST_CASE(replace_empty_to_empty, Fixture)
 	{
+		expected = source;
 		auto result = FindAndReplace(source, "", "");
-		BOOST_CHECK_EQUAL_COLLECTIONS(source.begin(), source.end(), result.begin(), result.end());
+		BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), result.begin(), result.end());
 	}
 
 	BOOST_FIXTURE_TEST_CASE(replace_existed_to_empty, Fixture)
 	{
+		expected = "soimes happy, soimes sad";
 		auto result = FindAndReplace(source, "met", "");
-		std::string expected = "soimes happy, soimes sad";
 		BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), result.begin(), result.end());
 	}
 
 
 	BOOST_FIXTURE_TEST_CASE(replace_not_existed_to_empty, Fixture)
 	{
+		expected = source;
 		auto result = FindAndReplace(source, "merch", "");
-		BOOST_CHECK_EQUAL_COLLECTIONS(source.begin(), source.end(), result.begin(), result.end());
+		BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), result.begin(), result.end());
 	}
 
 	BOOST_FIXTURE_TEST_CASE(replace_not_existed_to_not_empty, Fixture)
 	{
+		expected = source;
 		auto result = FindAndReplace(source, "merch", "asd");
-		BOOST_CHECK_EQUAL_COLLECTIONS(source.begin(), source.end(), result.begin(), result.end());
+		BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), result.begin(), result.end());
 	}
 
 	BOOST_FIXTURE_TEST_CASE(replace_existed_to_equalSize, Fixture)
 	{
+		expected = "soremimes happy, soremimes sad";
 		auto result = FindAndReplace(source, "met", "rem");
-		std::string expected = "soremimes happy, soremimes sad";
 		BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), result.begin(), result.end());
 	}
 
 	BOOST_FIXTURE_TEST_CASE(replace_existed_to_lessSize, Fixture)
 	{
+		expected = "soreimes happy, soreimes sad";
 		auto result = FindAndReplace(source, "met", "re");
-		std::string expected = "soreimes happy, soreimes sad";
 		BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), result.begin(), result.end());
 	}
 
 	BOOST_FIXTURE_TEST_CASE(replace_existed_to_moreSize, Fixture)
 	{
+		expected = "soremmimes happy, soremmimes sad";
 		auto result = FindAndReplace(source, "met", "remm");
-		std::string expected = "soremmimes happy, soremmimes sad";
 		BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), result.begin(), result.end());
 	}
 
 	BOOST_FIXTURE_TEST_CASE(replace_in_empty, Fixture)
 	{
 		source = "";
+		expected = source;
 		auto result = FindAndReplace(source, "met", "remm");
-		BOOST_CHECK_EQUAL_COLLECTIONS(source.begin(), source.end(), result.begin(), result.end());
+		BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), result.begin(), result.end());
 	}
 
 
 	BOOST_FIXTURE_TEST_CASE(replace_1231234_in_12312312345, Fixture)
 	{
 		source = "12312312345";
-		std::string expected = "123paste5";
+		expected = "123paste5";
 		auto result = FindAndReplace(source,"1231234", "paste");
 		BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), result.begin(), result.end());
 	}
@@ -85,7 +91,7 @@ BOOST_AUTO_TEST_SUITE(FindAndReplace_test)
 	BOOST_FIXTURE_TEST_CASE(replace_ma_in_mama, Fixture)
 	{
 		source = "mamampapa";
-		std::string expected = "pastepastempapa";
+		expected = "pastepastempapa";
 		auto result = FindAndReplace(source, "ma", "paste");
 		BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), result.begin(), result.end());
 	}
