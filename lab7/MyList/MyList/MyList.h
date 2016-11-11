00636b6e0bae59e4c03a5b5b99b9519d6e627f75
Sasha Ocheev <OcheevSasha@gmail.com>
2016-11-11 07:41:06 +0300

    list

--- lab7/MyList/MyList/MyList.h
+++ lab7/MyList/MyList/MyList.h
@@ -1,114 +1,254 @@
 #pragma once
-
+#include "stdafx.h"
+#include <string>
 #include <memory>
 
-template <typename T>
+template<typename T>
 class CMyList
 {
-private:
-	struct Node
+	struct SNode
 	{
-		Node *prev;
-		T data;
-		std::unique_ptr<Node> next;
+		SNode(const T & data, SNode * prev, std::unique_ptr<SNode> && next) : data(data), prev(prev), next(std::move(next)) {}
 
-		Node(const T & data, Node * prev, std::unique_ptr<Node> && next)
-			: data(data)
-			, prev(prev)
-			, next(std::move(next))
-		{
-		}
+		T data;
+		SNode *prev;
+		std::unique_ptr<SNode> next;
 	};
 
 	template<typename U>
-	class CIterator : std::iterator <std::bidirectional_iterator_tag, U>
+	class CListIterator : public std::iterator <std::random_access_iterator_tag, U>
 	{
 	public:
-		friend CMyList<T>;
-
-		CIterator(Node *node) :
-			m_node(node)
+		CListIterator(SNode* value, bool isReverse)
+			: m_node(value)
+			, m_isReverse(isReverse)
 		{
 		}
 
-		U& operator*()
+		friend class CMyList<T>;
+
+		bool operator!=(CListIterator const& other) const
 		{
-			
+			return m_node != other.m_node;
+		}
+		bool operator==(CListIterator const& other) const
+		{
+			return m_node == other.m_node;
 		}
 
-		bool operator==(const CIterator &other) const
+		U & operator*() const
 		{
-			return  other.m_node == m_node;
+			return m_node->data;
 		}
 
-		bool operator!=(const CIterator &other) const
+		CListIterator& operator++()
 		{
-			return other.m_node != m_node;
-		}
-
-		CIterator& operator++()
-		{
-			m_node = m_node->next.get()
+			if (m_isReverse)
+			{
+				m_node = m_node->prev;
+			}
+			else
+			{
+				m_node = m_node->next.get();
+			}
 			return *this;
 		}
-
-		CIterator& operator++(int)
+		CListIterator& operator--()
 		{
-			auto returnIt = this;
-			m_node = m_node->next.get();
-			return *returnIt;
-		}
-
-		CIterator& operator--()
-		{
-			m_node = m_node->prev;
+			if (m_isReverse)
+			{
+				m_node = m_node->next.get();
+			}
+			else
+			{
+				m_node = m_node->prev;
+			}
 			return *this;
-		}
-		
-		CIterator& operator--(int)
-		{
-			auto returnIt = this;
-			m_node = m_node->prev;
-			return *returnIt;
 		}
 
 	private:
-		Node *m_node = nullptr;
+		SNode* operator->()const
+		{
+			return m_node;
+		}
+
+		SNode* m_node = nullptr;
+		bool m_isReverse = b false;
 	};
 
-	typedef CIterator<T> Iterator;
-	typedef CIterator<const T> ConstIterator;
+	typedef CListIterator<T> ListIterator;
+	typedef CListIterator<const T> ConstListIterator;
 
 public:
 	CMyList() = default;
+	CMyList(CMyList & list)
+	{
+		CMyList tmp;
+		for (auto const& elem : list)
+		{
+			tmp.PushBack(elem);
+		}
+		std::swap(m_firstNode, tmp.m_firstNode);
+		std::swap(m_lastNode, tmp.m_lastNode);
+		m_size = tmp.m_size;
+	}
+
+	~CMyList()
+	{
+		Clear();
+	}
 
 	size_t GetSize()
 	{
 		return m_size;
 	}
 
-	Iterator begin()
+	void PushBack(const T & data)
 	{
-		return Iterator(m_first);
+		auto newNode = std::make_unique<SNode>(data, m_lastNode, nullptr);
+		SNode *newLastNode = newNode.get();
+		if (m_lastNode)
+		{
+			m_lastNode->next = std::move(newNode);
+		}
+		else
+		{
+			m_firstNode = std::move(newNode);
+		}
+		m_lastNode = newLastNode;
+		m_lastNode->next = nullptr;
+		++m_size;
+	}
+	void PushFront(const T & data)
+	{
+		auto newNode = std::make_unique<SNode>(data, nullptr, std::move(m_firstNode));
+		if (newNode->next)
+		{
+			newNode->next->prev = newNode.get();
+		}
+		else
+		{
+			m_lastNode = newNode.get();
+		}
+		m_firstNode = std::move(newNode);
+		m_firstNode->prev = nullptr;
+		m_size++;
 	}
 
-	ConstIterator begin() const
+	void Clear()
 	{
-		return ConstIterator(m_first);
+		while (m_lastNode)
+		{
+			m_lastNode->next = nullptr;
+			m_lastNode = m_lastNode->prev;
+		}
+		m_firstNode = nullptr;
+		m_size = 0;
+	}
+	bool IsEmpty() const
+	{
+		return m_size == 0u;
 	}
 
-	Iterator end()
+	T & GetBackElement()
 	{
-		return Iterator(m_last);
+		return m_lastNode->data;
+	}
+	const T & GetBackElement() const
+	{
+		return m_lastNode->data;
+	}
+	T & GetFrontElement()
+	{
+		return m_firstNode->data;
+	}
+	const T & GetFrontElement() const
+	{
+		return m_firstNode->data;
 	}
 
-	ConstIterator end() const
+	ListIterator begin()
 	{
-		return ConstIterator(m_last);
+		return ListIterator(m_firstNode.get(), false);
+	}
+	ListIterator end()
+	{
+		return ListIterator(m_lastNode->next.get(), false);
+	}
+	const ConstListIterator cbegin() const
+	{
+		return ConstListIterator(m_firstNode.get(), false);
+	}
+	const ConstListIterator cend() const
+	{
+		return ConstListIterator(m_lastNode->next.get(), false);
+	}
+	ListIterator rbegin()
+	{
+		return ListIterator(m_lastNode, true);
+	}
+	ListIterator rend()
+	{
+		return ListIterator(m_firstNode->prev, true);
+	}
+	const ConstListIterator crbegin() const
+	{
+		return ConstListIterator(m_lastNode, true);
+	}
+	const ConstListIterator crend() const
+	{
+		return ConstListIterator(m_firstNode->prev, true);
+	}
+
+	void Insert(const ListIterator & it, T data)
+	{
+		if (it == begin())
+		{
+			PushFront(data);
+		}
+		else if (it == end())
+		{
+			PushBack(data);
+		}
+		else
+		{
+			auto node = std::make_unique<SNode>(data, it->prev, std::move(it->prev->next));
+			it->prev = std::move(node.get());
+			node->prev->next = std::move(node);
+		}
+	}
+	void Erase(const ListIterator & it)
+	{
+		if (m_size == 1)
+		{
+			Clear();
+			return;
+		}
+
+		if (it == begin())
+		{
+			it->next->prev = nullptr;
+			m_firstNode = move(it->next);
+		}
+		else if (it->data == GetBackElement())
+		{
+			it->prev->next = nullptr;
+			m_lastNode = std::move(it->prev);
+		}
+		else
+		{
+			it->next->prev = std::move(it->prev);
+			it->prev->next = std::move(it->next);
+		}
+
+		if (m_size > 0)
+		{
+			m_size--;
+		}
 	}
 
 private:
-	std::unique_ptr<Node> m_first = nullptr;
-	Node *m_last = nullptr;
 	size_t m_size = 0;
+	std::unique_ptr<SNode> m_firstNode = nullptr;
+	SNode * m_lastNode = nullptr;
 };
--- lab7/MyList/Test/MyListTest.cpp
+++ lab7/MyList/Test/MyListTest.cpp
@@ -1,33 +1,186 @@
 #include "stdafx.h"
 #include "../MyList/MyList.h"
 
-struct ListItem
+struct EmptyStringList
 {
-	ListItem(int value = 0) : value(value)
-	{}
-	int value;
+	CMyList<std::string> list;
 };
 
-struct EmptyListItemList
+template<typename T>
+void VerifyList(CMyList<T> & list, std::vector<T> & expected)
 {
-	CMyList<ListItem> list;
-};
+	size_t counter = 0;
+	for (auto it = list.cbegin(); it != list.cend(); ++it)
+	{
+		BOOST_CHECK_EQUAL(*it, expected[counter]);
+		counter++;
+	}
+}
 
-BOOST_FIXTURE_TEST_SUITE(MyListTest, EmptyListItemList)
-	BOOST_AUTO_TEST_SUITE(by_default)
-		BOOST_AUTO_TEST_CASE(has_0_size)
+BOOST_FIXTURE_TEST_SUITE(CMyList_, EmptyStringList)
+
+	BOOST_AUTO_TEST_SUITE(after_created)
+
+		BOOST_AUTO_TEST_CASE(is_empty)
 		{
-			BOOST_CHECK_EQUAL(list.GetSize(), 0u);
+			BOOST_CHECK(list.IsEmpty());
 		}
+
 	BOOST_AUTO_TEST_SUITE_END()
-	BOOST_AUTO_TEST_SUITE(after_appending_an_item)
-		BOOST_AUTO_TEST_CASE(increases_its_size_and_capacity)
+
+	BOOST_AUTO_TEST_SUITE(after_push_back)
+
+		BOOST_AUTO_TEST_CASE(size_increases_by_1)
 		{
+			auto oldSize = list.GetSize();
+			list.PushBack("hello");
+			BOOST_CHECK_EQUAL(list.GetSize(), oldSize + 1);
+			list.PushBack("hello");
+			BOOST_CHECK_EQUAL(list.GetSize(), oldSize + 2);
+		}
+
+		BOOST_AUTO_TEST_CASE(accessible_via_GetBackElement_and_GetFrontElement_method)
+		{
+			list.PushBack("hello");
+			BOOST_CHECK_EQUAL(list.GetBackElement(), "hello");
+			BOOST_CHECK_EQUAL(list.GetFrontElement(), "hello");
+			list.PushBack("goodbye");
+			BOOST_CHECK_EQUAL(list.GetBackElement(), "goodbye");
+			BOOST_CHECK_EQUAL(list.GetFrontElement(), "hello");
+		}
+
+	BOOST_AUTO_TEST_SUITE_END()
+
+	BOOST_AUTO_TEST_SUITE(after_push_front)
+
+		BOOST_AUTO_TEST_CASE(size_increases_by_1)
+		{
+			auto oldSize = list.GetSize();
+			list.PushFront("hello");
+			BOOST_CHECK_EQUAL(list.GetSize(), oldSize + 1);
+			list.PushFront("hello");
+			BOOST_CHECK_EQUAL(list.GetSize(), oldSize + 2);
+		}
+
+		BOOST_AUTO_TEST_CASE(accessible_via_GetBackElement_and_GetFrontElement_method)
+		{
+			list.PushFront("hello");
+			BOOST_CHECK_EQUAL(list.GetFrontElement(), "hello");
+			BOOST_CHECK_EQUAL(list.GetBackElement(), "hello");
+			list.PushFront("goodbye");
+			BOOST_CHECK_EQUAL(list.GetFrontElement(), "goodbye");
+			BOOST_CHECK_EQUAL(list.GetBackElement(), "hello");
+		}
+
+		BOOST_AUTO_TEST_CASE(accessible_via_iterator_to_first_element)
+		{
+			list.PushBack("hello");
+			auto it = list.begin();
+			BOOST_CHECK_EQUAL(addressof(*it), addressof(list.GetBackElement()));
+		}
+
+	BOOST_AUTO_TEST_SUITE_END()
+
+	struct when_not_empty : EmptyStringList
+	{
+		when_not_empty()
+		{
+			list.PushBack("hello");
+			list.PushBack("ho-ho-ho");
+			list.PushBack("goodbye");
+		}
+
+		std::vector<std::string> expectedStrings = { "hello", "ho-ho-ho", "goodbye" };
+	};
+
+	BOOST_FIXTURE_TEST_SUITE(when_not_empty_, when_not_empty)
+
+		BOOST_AUTO_TEST_CASE(can_be_cleared)
+		{
+			BOOST_CHECK(!list.IsEmpty());
+			list.Clear();
+			BOOST_CHECK(list.IsEmpty());
+		}
+
+		BOOST_AUTO_TEST_CASE(has_iterators_and_const_iterators_at_begin_and_end)
+		{
+			size_t counter = 0;
+			for (auto it = list.begin(); it != list.end(); ++it)
+			{
+				BOOST_CHECK_EQUAL(*it, expectedStrings[counter]);
+				counter++;
+			}
+
+			counter = 0;
+			for (auto it = list.cbegin(); it != list.cend(); ++it)
+			{
+				BOOST_CHECK_EQUAL(*it, expectedStrings[counter]);
+				counter++;
+			}
+		}
+
+		BOOST_AUTO_TEST_CASE(has_reverse_iterators_and_const_revers_iterators_at_begin_and_end)
+		{
+			size_t counter = expectedStrings.size() - 1;
+			for (auto it = list.rbegin(); it != list.rend(); ++it)
+			{
+				BOOST_CHECK_EQUAL(*it, expectedStrings[counter]);
+				counter--;
+			}
+		}
+
+		BOOST_AUTO_TEST_CASE(can_acces_to_elements_from_range_based_for)
+		{
+			size_t counter = 0;
+			for (auto str : list)
+			{
+				BOOST_CHECK_EQUAL(str, expectedStrings[counter]);
+				counter++;
+			}
+		}
+
+		BOOST_AUTO_TEST_CASE(can_insert_element_at_iterator_pos)
+		{
+			auto it = ++list.begin();
+			list.Insert(it, "20");
+			BOOST_CHECK_EQUAL(*++list.begin(), "20");
+
+			list.Insert(list.begin(), "11");
+			BOOST_CHECK_EQUAL(*list.begin(), "11");
+
+			list.Insert(list.end(), "30");
+
+			std::vector<std::string> expectedStrings = {"11", "hello", "20", "ho-ho-ho", "goodbye", "30"};
+			size_t i = 0;
+			for (auto str : list)
+			{
+				BOOST_CHECK_EQUAL(str, expectedStrings[i]);
+				i++;
+			}
 			
 		}
-		BOOST_AUTO_TEST_CASE(it_becomes_available_at_the_back)
+
+		BOOST_AUTO_TEST_CASE(can_erase_element_at_iterator_pos)
 		{
-			
+			auto it = ++list.begin();
+			BOOST_CHECK_EQUAL(*it, "ho-ho-ho");
+			list.Erase(it);
+			BOOST_CHECK_EQUAL(*++list.begin(), "goodbye");
+
+			list.Erase(list.begin());
+			BOOST_CHECK_EQUAL(*list.begin(), "goodbye");
+
+			list.Erase(list.begin());
+			BOOST_CHECK(list.IsEmpty());
 		}
+
+		BOOST_AUTO_TEST_CASE(have_copy_constructor)
+		{
+			CMyList<std::string> list1(list);
+			VerifyList(list, expectedStrings);
+			VerifyList(list1, expectedStrings);
+		}
+
 	BOOST_AUTO_TEST_SUITE_END()
+
 BOOST_AUTO_TEST_SUITE_END()
