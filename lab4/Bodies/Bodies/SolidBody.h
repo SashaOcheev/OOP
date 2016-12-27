33652bf3fcdc91071419924ddc19ebb4688a14f7
Sasha Ocheev <OcheevSasha@gmail.com>
2016-12-27 20:57:42 +0300

    Выпилен override = 0

--- lab4/Bodies/Bodies/SolidBody.h
+++ lab4/Bodies/Bodies/SolidBody.h
@@ -10,11 +10,8 @@ public:
 	CSolidBody(const std::string &type, double density);
 
 	double GetDensity() const final;
-	double GetVolume() const override = 0;
 	double GetMass() const final;
 	~CSolidBody() override;
-protected:
-	void AppendProperties(std::ostream & strm) const override = 0;
 private:
 	double m_density;
 };
